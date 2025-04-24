aaff6741f666706ca2e35b5fb14b32d62974ea99
Minh Quan Tran <62203526+MinhQuan-Tran@users.noreply.github.com>
2025-04-24 22:10:51 +1000

    Update firebase.ts

--- focusmates/src/firebase.ts
+++ focusmates/src/firebase.ts
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,3 +16,11 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+const appCheck = initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider("6Ldq_iIrAAAAABzkCR0ASLL7u5QfVW8AMbVpALPN"),
+
+  // Optional argument. If true, the SDK automatically refreshes App Check
+  // tokens as needed.
+  isTokenAutoRefreshEnabled: true
+});
