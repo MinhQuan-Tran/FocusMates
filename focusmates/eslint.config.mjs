028a9f40744ff70f323ab5577efa345a4b985275
Minh Quan Tran <62203526+MinhQuan-Tran@users.noreply.github.com>
2025-04-26 18:06:25 +1000

    Update eslint.config.mjs

--- focusmates/eslint.config.mjs
+++ focusmates/eslint.config.mjs
@@ -13,10 +13,7 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     ignores: ["**/*"], // Ignore all files
-    rules: {
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-unused-vars": "off",
-    },
+    rules: {}, // Disable all rules
   },
 ];
 
